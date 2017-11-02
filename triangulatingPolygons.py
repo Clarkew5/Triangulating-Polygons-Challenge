190338200c2b114edff24bcfdb3abf4e593c9ec4
ClarkeW5 <clarkew5@mymacewan.ca>
2017-11-02 17:50:22 -0600

    replaces colourless with blank to shorten longer lines, and refactoring other lines

--- triangulatingPolygons.py
+++ triangulatingPolygons.py
@@ -1,7 +1,7 @@
 import sys
 
 class Vertex:
-    def __init__(self, name="", colour="uncoloured", connectedVertices=set()):
+    def __init__(self, name="", colour="blank", connectedVertices=set()):
         self.name = name
         self.colour = colour
         self.connectedVertices = connectedVertices
@@ -21,7 +21,7 @@ def testVertexConstructors():
         print("FAIL")
 
     print("\tDefault Colour:\t\t", end="")
-    if (defaultV.colour == "uncoloured"):
+    if (defaultV.colour == "blank"):
         print("PASS")
     else:
         print("FAIL")
@@ -69,28 +69,28 @@ def test():
 
 def main():
     polygon = dict()
-    polygon["A"] = Vertex("A", "red",           {"B","E","F"})
-    polygon["B"] = Vertex("B", "red",           {"A","C","F","G"})
-    polygon["C"] = Vertex("C", "yellow",        {"B","D","G","H"})
-    polygon["D"] = Vertex("D", "blue",          {"C","H","J"})
-    polygon["E"] = Vertex("E", "blue",          {"A","F","I","K"})
-    polygon["F"] = Vertex("F", "uncoloured",    {"B","E","F"})
-    polygon["G"] = Vertex("G", "uncoloured",    {"B","C","F","H","I","J","M","N"})
-    polygon["H"] = Vertex("H", "uncoloured",    {"C","D","G","J","N"})
-    polygon["I"] = Vertex("I", "uncoloured",    {"E","F","G","K","L","M"})
-    polygon["J"] = Vertex("J", "red",           {"D","H","N","O","S"})
-    polygon["K"] = Vertex("K", "yellow",        {"E","I","L","P","T"})
-    polygon["L"] = Vertex("L", "uncoloured",    {"I","K","M","P"})
-    polygon["M"] = Vertex("M", "uncoloured",    {"G","I","L","N","P","Q"})
-    polygon["N"] = Vertex("N", "uncoloured",    {"G","H","J","M","O","Q","R","S"})
-    polygon["O"] = Vertex("O", "uncoloured",    {"J","N","R","S"})
-    polygon["P"] = Vertex("P", "uncoloured",    {"K","L","M","T","Q"})
-    polygon["Q"] = Vertex("Q", "uncoloured",    {"M","N","P","R","T","V"})
-    polygon["R"] = Vertex("R", "uncoloured",    {"N","O","Q","S","U","V"})
-    polygon["S"] = Vertex("S", "yellow",        {"J","O","R","U"})
-    polygon["T"] = Vertex("T", "red",           {"K","R","S","V"})
-    polygon["U"] = Vertex("U", "blue",          {"Q","R","S","V"})
-    polygon["V"] = Vertex("V", "red",           {"Q", "R", "T", "U"})
+    polygon["A"] = Vertex("A", "red",       {"B","E","F"})
+    polygon["B"] = Vertex("B", "red",       {"A","C","F","G"})
+    polygon["C"] = Vertex("C", "yellow",    {"B","D","G","H"})
+    polygon["D"] = Vertex("D", "blue",      {"C","H","J"})
+    polygon["E"] = Vertex("E", "blue",      {"A","F","I","K"})
+    polygon["F"] = Vertex("F", "blank",     {"B","E","F"})
+    polygon["G"] = Vertex("G", "blank",     {"B","C","F","H","I","J","M","N"})
+    polygon["H"] = Vertex("H", "blank",     {"C","D","G","J","N"})
+    polygon["I"] = Vertex("I", "blank",     {"E","F","G","K","L","M"})
+    polygon["J"] = Vertex("J", "red",       {"D","H","N","O","S"})
+    polygon["K"] = Vertex("K", "yellow",    {"E","I","L","P","T"})
+    polygon["L"] = Vertex("L", "blank",     {"I","K","M","P"})
+    polygon["M"] = Vertex("M", "blank",     {"G","I","L","N","P","Q"})
+    polygon["N"] = Vertex("N", "blank",     {"G","H","J","M","O","Q","R","S"})
+    polygon["O"] = Vertex("O", "blank",     {"J","N","R","S"})
+    polygon["P"] = Vertex("P", "blank",     {"K","L","M","T","Q"})
+    polygon["Q"] = Vertex("Q", "blank",     {"M","N","P","R","T","V"})
+    polygon["R"] = Vertex("R", "blank",     {"N","O","Q","S","U","V"})
+    polygon["S"] = Vertex("S", "yellow",    {"J","O","R","U"})
+    polygon["T"] = Vertex("T", "red",       {"K","R","S","V"})
+    polygon["U"] = Vertex("U", "blue",      {"Q","R","S","V"})
+    polygon["V"] = Vertex("V", "red",       {"Q","R","T","U"})
 
 if (__name__ == "__main__"):
     if (len(sys.argv) == 2 and sys.argv[1] == '-t'):
